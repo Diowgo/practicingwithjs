29b7f56bb42c3d5581ef1ead0761b030bed60971
Diowgo <Diogovds71@gmail.com>
2024-10-26 14:58:48 -0300

    Handling bugs

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-const button = document.querySelector("button");
+const buttons = document.querySelectorAll("button");
+const paragraph = document.getElementById("paragraph");
 
+
+function clickHandler() {
+    const name = prompt("What is your name?");
+    paragraph.innerText = `Hello, ${name}`;
+}
+
+for (const button of buttons) {
+    button.addEventListener("click", clickHandler);
+}
